bf1ac82141a1db95ccf888178c15e0c231aa8a2a
Hao Chen <mmkrty@gmail.com>
2021-03-20 21:09:55 +0100

    complete appending movies to DOM, adjust css.

--- script.js
+++ script.js
@@ -3,16 +3,51 @@ const IMG_PATH ='https://image.tmdb.org/t/p/w1280'
 const SEARCH_API ='https://api.themoviedb.org/3/search/movie?api_key=61725412b471f159ecdb4d2c34cacf01&query="'
 const form = document.getElementById("form")
 const search = document.getElementById("search")
+const main = document.getElementById("main")
 
 getMovies(API_URL);
-
 
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
 } 
+
+function showMovies(movies){
+  main.innerHTML = '';
+  movies.forEach( (movie) => {
+      const {title, poster_path, vote_average, overview} = movie
+
+      const movieEl = document.createElement('div')
+      movieEl.classList.add('movie');
+
+      movieEl.innerHTML = `
+            <img src="${IMG_PATH+poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getRateColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+      `
+      main.appendChild(movieEl)
+      
+  })
+}
+
+function getRateColor(rate) {
+    if(rate >= 8) {
+        return 'green'
+    } else if (rate >= 5){
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
